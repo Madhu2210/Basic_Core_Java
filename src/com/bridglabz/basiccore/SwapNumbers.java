549e199ca615875ca268e88b3d2c5632a23bccd1
Madhu Kahar <madhu.kahar2210@gmail.com>
2022-04-27 00:39:10 +0530

    Added: Swap the two numbers.

---
+++ src/com/bridglabz/basiccore/SwapNumbers.java
@@ -0,0 +1,22 @@
+package com.bridglabz.basiccore;
+import java.util.Scanner;
+
+public class SwapNumbers {
+    public static void main(String[] args) {
+        int team;
+        System.out.println("Enter the value of X and Y");
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        int y = sc.nextInt();
+        System.out.println("before swapping numbers: " + x + "  " + y);
+        System.out.println("Value of x:" + x);
+        System.out.println("Value of y:" + y);
+        team = x;
+        x = y;
+        y = team;
+        System.out.println("After swapping: " + x + "   " + y);
+        System.out.println("Value of x:" + x);
+        System.out.println("Value of y:" + y);
+    }
+}
+
