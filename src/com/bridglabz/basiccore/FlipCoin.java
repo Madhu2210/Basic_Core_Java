6dda95ceb452496ea39acff11c13a6c5ac8abec7
Madhu Kahar <madhu.kahar2210@gmail.com>
2022-04-26 16:50:13 +0530

    Added: Welcome to Basic core Program.

---
+++ src/com/bridglabz/basiccore/FlipCoin.java
@@ -0,0 +1,9 @@
+package com.bridglabz.basiccore;
+import java.util.Scanner;
+
+public class FlipCoin {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Basic Core Programs.");
+    }
+}
+
