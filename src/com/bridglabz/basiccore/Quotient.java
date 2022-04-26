37dfa18a185d161bb89092f43282d416ae1c1f3d
Madhu Kahar <madhu.kahar2210@gmail.com>
2022-04-26 18:53:44 +0530

    Added: Compute Quotient and Remainder

---
+++ src/com/bridglabz/basiccore/Quotient.java
@@ -0,0 +1,11 @@
+package com.bridglabz.basiccore;
+
+public class Quotient {
+    public static void main(String[] args) {
+        int dividend = 20, divisor = 5;
+        int quotient = dividend / divisor;
+        int remainder = dividend % divisor;
+        System.out.println("Quotient = " + quotient);
+        System.out.println("Remainder = " + remainder);
+    }
+}
